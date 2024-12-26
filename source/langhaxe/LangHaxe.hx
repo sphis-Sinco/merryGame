9049f4cfea88749d2ad3fc0d7cd1c77a4b12c772
IdealistCat <mattallan.h@gmail.com>
2024-12-26 13:00:01 -0600

    actually we do lol

---
+++ source/langhaxe/LangHaxe.hx
@@ -0,0 +1,57 @@
+package langhaxe;
+
+using StringTools;
+
+typedef LangHaxe =
+{
+	var name:String;
+	var ?asset_suffix:String;
+	var ?lang_ver:Float;
+	var ?phrases:haxe.DynamicAccess<String>;
+}
+
+class PhraseManager
+{
+	public static var PHRASES_REQUIRING_FALLBACK:Array<String> = [];
+
+	public static var languageList:LangHaxe = null;
+
+	public static function init()
+	{
+		// Avoid loading the language file everytime getPhrase is called.
+		try
+		{
+			languageList = Language.readLang(Preferences.savedata.language);
+		}
+		catch (e)
+		{
+			trace("Failed loading language: " + Preferences.savedata.language + " // " + e.message);
+			languageList = Language.readLang("english"); // Use english.
+			return;
+		}
+		trace("Language \"" + Preferences.savedata.language + "\" loaded.");
+	}
+
+	public static function getPhrase(phrase:Dynamic, ?fb:Dynamic = null):Dynamic
+	{
+		var json:haxe.DynamicAccess<String> = languageList.phrases;
+		var fallback:Dynamic = (fb != null ? fb : phrase);
+		var returnValue:Dynamic;
+
+		try
+		{
+			returnValue = json.get(Std.string(phrase).toLowerCase().replace(' ', '_'));
+		}
+		catch (e)
+		{
+			if (!PHRASES_REQUIRING_FALLBACK.contains(phrase))
+			{
+				trace('[PHRASE MANAGER] Phrase "$phrase" required fallback');
+				PHRASES_REQUIRING_FALLBACK.push(phrase);
+			}
+			returnValue = fallback;
+		}
+
+		return returnValue;
+	}
+}
